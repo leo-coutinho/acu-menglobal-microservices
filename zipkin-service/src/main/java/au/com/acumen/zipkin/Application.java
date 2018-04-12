ecd7dc0d2348591960f6e565eb5b27fb5c0a4c71
leo <leo.coutinho@acu-menglobal.com>
2018-04-12 14:46:09 +1000

    Distributed tracing system.

---
+++ zipkin-service/src/main/java/au/com/acumen/zipkin/Application.java
@@ -0,0 +1,15 @@
+package au.com.acumen.zipkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import zipkin.server.EnableZipkinServer;
+
+@SpringBootApplication
+@EnableZipkinServer
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
